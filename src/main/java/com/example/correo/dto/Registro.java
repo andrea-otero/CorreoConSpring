4b2831436782de1455451c081620c5ce2bce9927
andrea-otero <422032407@pcpuma.acatlan.unam.mx>
2025-04-06 00:08:02 -0600

    Create Registro.java

---
+++ src/main/java/com/example/correo/dto/Registro.java
@@ -0,0 +1,24 @@
+package com.example.correo.dto;
+
+public class Registro {
+	private String nombre;
+    private String correo;
+
+    // Getters y setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+}
