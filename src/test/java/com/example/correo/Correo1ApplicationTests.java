44434fb38f1b416539a0a6e294bac886840fea17
andrea-otero <422032407@pcpuma.acatlan.unam.mx>
2025-04-06 00:09:07 -0600

    Create Correo1ApplicationTests.java

---
+++ src/test/java/com/example/correo/Correo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.correo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Correo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
